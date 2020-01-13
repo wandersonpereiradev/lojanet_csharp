fc6dd2148a6e1f1cb533be2f56887c1f2a4b781f
Wanderson Pereira <wanderson.p.ayres@gmail.com>
2020-01-13 17:11:45 -0300

    Criando Interface / Class Cliente

---
+++ LojaNet.DAL/ClienteDAL.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LojaNet.Models;
+
+namespace LojaNet.DAL
+{
+    public class ClienteDAL : IClienteDados
+    {
+        public void Alterar(Cliente cliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(string Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Incluir(Cliente cliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Cliente ObterPorEmail(string Email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Cliente ObterPorId(string Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Cliente> ObterTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LojaNet.Models/IClienteDados.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LojaNet.Models
+{
+    public interface IClienteDados
+    {
+        void Incluir(Cliente cliente);
+        void Alterar(Cliente cliente);
+        void Excluir(string Id);
+        List<Cliente> ObterTodos();
+        Cliente ObterPorId(string Id);
+        Cliente ObterPorEmail(string Email);
+    }
+}
