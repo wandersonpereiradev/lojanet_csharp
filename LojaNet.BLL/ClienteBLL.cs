2f3dde048d7c88b55f96313d206645d2a1223f73
Wanderson Pereira <wanderson.p.ayres@gmail.com>
2020-01-13 17:30:51 -0300

    Criando ClienteBLL

---
+++ LojaNet.BLL/ClienteBLL.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LojaNet.Models;
+using LojaNet.DAL;
+
+namespace LojaNet.BLL
+{
+    public class ClienteBLL : IClienteDados
+    {
+        public void Alterar(Cliente cliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(string Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Incluir(Cliente cliente)
+        {
+            if (string.IsNullOrEmpty(cliente.Nome))
+            {
+                throw new ApplicationException("O nome deve ser informado");
+            }
+            if (string.IsNullOrEmpty(cliente.Id))
+            {
+                // Criando o ID dinamicamente
+                cliente.Id = Guid.NewGuid().ToString();
+            }
+
+            var dal = new ClienteDAL();
+            dal.Incluir(cliente);
+        }
+
+        public Cliente ObterPorEmail(string Email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Cliente ObterPorId(string Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Cliente> ObterTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
